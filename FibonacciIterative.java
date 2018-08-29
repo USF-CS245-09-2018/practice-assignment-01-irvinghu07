113a956a22de42cf5ff69076f1207f283d26a60b
Irving Hu <31359459+irvinghu07@users.noreply.github.com>
2018-08-29 13:49:00 -0700

    Update FibonacciIterative.java

--- FibonacciIterative.java
+++ FibonacciIterative.java
@@ -1,6 +1,5 @@
 package edu.usfca.cs.cs245.classRequired;
 
-import edu.usfca.cs.cs245.Factorial;
 import edu.usfca.cs.cs245.Fibonacci;
 
 public class FibonacciIterative implements Fibonacci {
@@ -15,13 +14,16 @@ public class FibonacciIterative implements Fibonacci {
      */
     @Override
     public int fibonacci(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 1) {
             return 1;
         }
-        int firstNum = 1;
+        if (n == 0) {
+            return 0;
+        }
+        int firstNum = 0;
         int secondNum = 1;
         int newSum = 0;
-        for (int i = 2; i < n; i++) {
+        for (int i = 1; i < n; i++) {
             newSum = firstNum + secondNum;
             firstNum = secondNum;
             secondNum = newSum;
